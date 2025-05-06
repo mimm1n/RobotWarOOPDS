97ccca4510401a760eea437dec0484b5d3224128
Khayrin <khayrin@Khayrins-MacBook-Pro.local>
2025-05-06 22:54:26 +0800

    wrote my name

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@ Program: main.cpp
 Course: OOPDS
 Trimester: 2510
 Name: Aliyah, Khayrin, Aimi, Amirul
-ID: - , - , 243UC247CJ, - 
+ID: - , 243UC246KQ , 243UC247CJ, - 
 Lecture Section: TC2L
 Tutorial Section: TT7L
-Email: -, -, AIMI.MOHD.FAIZAL@student.mmu.edu.my, - 
-Phone: -, -, 0139071648
+Email: -, KHAYRIN.SOFIYA.JAMEL@student.mmu.edu.my, AIMI.MOHD.FAIZAL@student.mmu.edu.my, - 
+Phone: -, 0193320041, 0139071648
 **********|**********|**********/
 
 #include <iostream>
