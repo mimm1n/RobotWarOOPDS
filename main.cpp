e7ec563b5184bb2d25f16de45f861d3b19e52dae
Samihtisyam <amirulal371@gmail.com>
2025-05-11 21:44:36 +0800

    ,

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@ Program: main.cpp
 Course: OOPDS
 Trimester: 2510
 Name: Aliyah, Khayrin, Aimi, Amirul
-ID: 243UC24665 , 243UC246KQ , 243UC247CJ, -
+ID: 243UC24665 , 243UC246KQ , 243UC247CJ, 241UC24199
 Lecture Section: TC2L
 Tutorial Section: TT7L
-Email: NOR.ALIYAH.SYAHIRAH@student.mmu.edu.my, KHAYRIN.SOFIYA.JAMEL@student.mmu.edu.my, AIMI.MOHD.FAIZAL@student.mmu.edu.my, -
+Email: NOR.ALIYAH.SYAHIRAH@student.mmu.edu.my, KHAYRIN.SOFIYA.JAMEL@student.mmu.edu.my, AIMI.MOHD.FAIZAL@student.mmu.edu.my, amirul.ihitsyam.idrus@student.mmu.edu.my
 
-Phone: 0146202605, 0193320041, 0139071648
+Phone: 0146202605, 0193320041, 0139071648, 0194090095
 **********|**********|**********/
 
 #include <iostream>
