eabcf8da956a9671d48865781f4a73d790ca38a0
Khayrin <khayrin@Khayrins-MacBook-Pro.local>
2025-05-14 14:40:05 +0800

    robot class functions and variables

--- Robot.cpp
+++ Robot.cpp
@@ -1,5 +1,46 @@
 #include "Robot.h"
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
+Robot(int robotX, int robotY, string name, string type){
+    setRobotX(robotX);
+    setRobotY(robotY);
+    setRobotName(name);
+    setRobotType(type);
+}
+
+virtual ~Robot(){}
+
+void setRobotX(int x){
+    robotX = x;
+}
+
+int getRobotX() const{
+    return robotX;
+}
+
+void setRobotY(int y){
+    robotY = y;
+}
+
+int getRobotY() const{
+    return robotY;
+}
+
+void setRobotName(string name){
+    robotName = name;
+}
+
+string getRobotName() const{
+    return robotName;
+}
+
+void setRobotType(string type){
+    robotType = type;
+}
+
+string getRobotType() const{
+    return robotType;
+}
