b4a4b25376e91f02f52193ab7fb0f65e35c2b668
Khayrin <khayrin@Khayrins-MacBook-Pro.local>
2025-05-16 12:14:33 +0800

    added functions and code header

--- Robot.cpp
+++ Robot.cpp
@@ -1,12 +1,24 @@
+/**********|**********|**********|
+Program: Robot.cpp
+Course: OOPDS
+Trimester: 2510
+Name: Aliyah, Khayrin, Aimi, Amirul
+ID: 243UC24665 , 243UC246KQ , 243UC247CJ, 241UC24199
+Lecture Section: TC2L
+Tutorial Section: TT7L
+Email: NOR.ALIYAH.SYAHIRAH@student.mmu.edu.my, KHAYRIN.SOFIYA.JAMEL@student.mmu.edu.my, AIMI.MOHD.FAIZAL@student.mmu.edu.my, AMIRUL.IHTISYAM.IDRUS@student.mmu.edu.my
+Phone: 0146202605, 0193320041, 0139071648, 0194090095
+**********|**********|**********/
+
 #include "Robot.h"
 #include <iostream>
 #include <cstdlib>
 #include <string>
 using namespace std;
 
-Robot::Robot(int robotX, int robotY, string name, string type){
-    setRobotX(robotX);
-    setRobotY(robotY);
+Robot::Robot(int x, int y, string name, string type){
+    setRobotX(x);
+    setRobotY(y);
     robotName = name;
     robotType = type;
     // setRobotName(name);
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ Name: Aliyah, Khayrin, Aimi, Amirul
 ID: 243UC24665 , 243UC246KQ , 243UC247CJ, 241UC24199
 Lecture Section: TC2L
 Tutorial Section: TT7L
-Email: NOR.ALIYAH.SYAHIRAH@student.mmu.edu.my, KHAYRIN.SOFIYA.JAMEL@student.mmu.edu.my, AIMI.MOHD.FAIZAL@student.mmu.edu.my, amirul.ihtisyam.idrus@student.mmu.edu.my
+Email: NOR.ALIYAH.SYAHIRAH@student.mmu.edu.my, KHAYRIN.SOFIYA.JAMEL@student.mmu.edu.my, AIMI.MOHD.FAIZAL@student.mmu.edu.my, AMIRUL.IHTISYAM.IDRUS@student.mmu.edu.my
 Phone: 0146202605, 0193320041, 0139071648, 0194090095
 **********|**********|**********/
 
