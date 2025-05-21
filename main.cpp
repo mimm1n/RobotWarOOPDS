9f1ea9f84919719cddc02415df98312819bf647c
Samihtisyam <amirulal371@gmail.com>
2025-05-21 18:47:50 +0800

    upgraded robot

--- main.cpp
+++ main.cpp
@@ -41,6 +41,27 @@ class ShootingRobot : public Robot{
 
 int main() {
     cout << "Hello World!" << endl;
+
+    ScoutBot scout;
+    scout.actionLook(&field);
+
+    TrackBot track;
+    track.actionLook(&field);
+
+    LongShotBot longShot;
+    longShot.actionFire(&field);
+
+    SemiAutoBot semiAuto;
+    semiAuto.actionFire(&field);
+
+    ThirtyShotBot thirtyShot;
+    thirtyShot.actionFire(&field);
+
+    HideBot hide;
+    hide.actionMove(&field);
+
+    JumpBot jump;
+    jump.actionMove(&field);
 }
 
 /* FUNCTION DEFINITIONS */
@@ -63,3 +84,60 @@ void ShootingRobot::setLocation(int x, int y){
     setRobotX(x);
     setRobotY(y);
 }
+
+class ScoutBot : public Robot,
+                 public SeeingRobot<ScoutBot> {
+public:
+    void doLook(Battlefield* battlefield) {
+        cout << "ScoutBot is scanning the area..." <<endl; // i don't know this is right or wrong
+        battlefield->status();
+    }
+};
+
+class TrackBot : public Robot,
+                 public SeeingRobot<TrackBot> {
+public:
+    void doLook(Battlefield* battlefield) {
+        std::cout << "TrackBot is planting a tracker on an enemy robot..." <<endl; // i don't know this is right or wrong
+    }
+};
+
+class LongShotBot : public Robot,
+                    public ShootingRobot<LongShotBot> {
+public:
+    void doFire(Battlefield* battlefield) {
+        cout << "LongShotBot is firing up to three units distance away..." <<endl; // i don't know this is right or wrong
+    }
+};
+
+class SemiAutoBot : public Robot,
+                    public ShootingRobot<SemiAutoBot> {
+public:
+    void doFire(Battlefield* battlefield) {
+        cout << "SemiAutoBot is firing three consecutive shells into one location..." <<endl; // i don't know this is right or wrong
+    }
+};
+
+class ThirtyShotBot : public Robot,
+                      public ShootingRobot<ThirtyShotBot> {
+public:
+    void doFire(Battlefield* battlefield) {
+        cout << "ThirtyShotBot is firing with a fresh load of 30 shells..." <<endl; // i don't know this is right or wrong
+    }
+};
+
+class HideBot : public Robot,
+                public MovingRobot<HideBot> {
+public:
+    void doMove(Battlefield* battlefield) {
+        cout << "HideBot is hiding for three turns..." << endl; // i don't know this is right or wrong
+    }
+};
+
+class JumpBot : public Robot,
+                public MovingRobot<JumpBot> {
+public:
+    void doMove(Battlefield* battlefield) {
+        cout << "JumpBot is jumping to a new location..." <<endl; // i don't know this is right or wrong
+    }
+};
